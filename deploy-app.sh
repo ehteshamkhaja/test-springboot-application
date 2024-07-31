a04ce67d1af4acfba268b9cdceb9d3d1693c3ed6
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2024-07-31 15:05:14 +0300

    Update deploy-app.sh

--- deploy-app.sh
+++ deploy-app.sh
@@ -2,5 +2,9 @@
 
 cluster_name=dev-eks-cluster
 aws eks update-kubeconfig --name $cluster_name --region us-west-1
+
 helmrelease="springboot-app"
+helm repo add eks https://aws.github.io/eks-charts
+helm repo update eks
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller  -n kube-system --set clusterName=$cluster_name  --set serviceAccount.create=false  --set serviceAccount.name=aws-load-balancer-controller --set region=us-west-1 --set vpcId=vpc-06a9dc1f8d2f9a5b1
 cd manifests && helm install $helmrelease myspringboot-app  
