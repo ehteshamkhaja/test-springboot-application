b32af2c2ef0c644ee6e783c1dcc882e636ef5f4b
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2024-07-31 16:07:46 +0300

    Update deploy-app.sh

--- deploy-app.sh
+++ deploy-app.sh
@@ -13,7 +13,7 @@ echo $lbhelmname
 cd manifests && helm uninstall $helmrelease && helm install $helmrelease myspringboot-app
 
 
-helm install $lbhelm eks/aws-load-balancer-controller  -n kube-system --set clusterName=$cluster_name  --set serviceAccount.create=false  --set serviceAccount.name=aws-load-balancer-controller --set region=us-west-1 --set vpcId=vpc-06a9dc1f8d2f9a5b1
+helm uninstall $lbhelm && helm install $lbhelm eks/aws-load-balancer-controller  -n kube-system --set clusterName=$cluster_name  --set serviceAccount.create=false  --set serviceAccount.name=aws-load-balancer-controller --set region=us-west-1 --set vpcId=vpc-06a9dc1f8d2f9a5b1
 
 
 
