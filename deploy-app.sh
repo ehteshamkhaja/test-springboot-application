733d23e122c5977ffc4f14192aefb9687ec23497
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2024-07-31 11:21:01 +0300

    Update deploy-app.sh

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cluster_name=dev-eks-cluster
+aws eks update-kubeconfig --name $cluster_name --region us-west-1
 helmrelease="springboot-app"
 cd manifests && helm install $helmrelease myspringboot-app  
