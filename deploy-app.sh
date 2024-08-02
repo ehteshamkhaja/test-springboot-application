63ccdde83d6d83111ae237ef712205ba6f4ab30e
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2024-08-02 03:01:38 +0300

    Update deploy-app.sh

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 cluster_name=test-eks-cluster
-aws eks update-kubeconfig --name $cluster_name --region us-west-1
+region=us-east-2
 
-helmrelease="springboot-app"
+aws eks update-kubeconfig --name $cluster_name --region $region
+
+releasename=spring
+
+
 helm repo add eks https://aws.github.io/eks-charts
 helm repo update eks
 
@@ -12,28 +16,8 @@ helmcheck=`helm list | grep $helmrelease | awk -F ' ' '{print $1}'`
 if [[ -z $helmcheck ]]
 then
   
-  cd manifests  && helm install $helmrelease myspringboot-app
+  cd manifests  && helm install $releasename myspringboot-app
 else 
    helm uninstall $helmrelease
-   cd manifests  && helm install $helmrelease myspringboot-app
+   cd manifests  && helm install $releasename myspringboot-app
 fi 
-
-#lbhelm=aws-load-balancer-controller
-#lbhelmname=`helm list | grep $lbhelm`
-#echo $lbhelmname
-#albcheck=`helm list | grep $lbhelmname`
-
-#if [ -z $albcheck ]
-#then 
-  
-#  helm install $lbhelm eks/aws-load-balancer-controller  -n kube-system --set clusterName=$cluster_name  --set serviceAccount.create=false  --set serviceAccount.name=aws-load-balancer-controller --set region=us-west-1 --set vpcId=vpc-06a9dc1f8d2f9a5b1
-#else
-#  helm uninstall $lbhelm
-#  helm install $lbhelm eks/aws-load-balancer-controller  -n kube-system --set clusterName=$cluster_name  --set serviceAccount.create=false  --set serviceAccount.name=aws-load-balancer-controller --set region=us-west-1 --set vpcId=vpc-06a9dc1f8d2f9a5b1
-#fi 
-
-# helm install aws-load-balancer-controller eks/aws-load-balancer-controller  -n kube-system --set clusterName=$cluster_name  --set serviceAccount.create=false  --set serviceAccount.name=aws-load-balancer-controller --set region=us-west-1 --set vpcId=vpc-06a9dc1f8d2f9a5b1
-
-
-
-  
